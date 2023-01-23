14fcac1b4322bba231b60168d67f17f8eda7526a
Alex <ais_alex@ukr.net>
2023-01-23 05:06:00 +0200

    summary

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,20 @@
 import { Component } from 'react';
+import { ContactForm } from 'components/ContactForm/ContactForm';
 
 export class App extends Component {
   state = {
     contacts: [],
-    name: '',
-    number: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
   };
 
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <form action="">
-          <label htmlFor="">Name</label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <button type="submit">add contact</button>
-        </form>
+        <ContactForm onSubmit={this.formSubmitHandler} />
         <div>
           <h1>Contacts</h1>
           <ul></ul>
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = nanoid();
+  telInputId = nanoid();
+
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form action="" onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameInputId}>Name</label>
+        <input
+          type="text"
+          name="name"
+          id={this.nameInputId}
+          value={this.state.name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleInputChange}
+        />
+        <button type="submit">add contact</button>
+      </form>
+    );
+  }
+}
